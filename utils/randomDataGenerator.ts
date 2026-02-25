075559da879ed64fa89cea5934dd496318cea72e
Narmadha7 <narmathat7148@gmail.com>
2026-02-25 20:52:38 +0530

    formetted the file

--- utils/randomDataGenerator.ts
+++ utils/randomDataGenerator.ts
@@ -1,54 +1,48 @@
 import { faker } from '@faker-js/faker';
 
-export class RandomDataUtil{
+export class RandomDataUtil {
 
-static getFirstName()
-{
+  static getFirstName() {
     return faker.person.firstName();
 
-}
+  }
 
-static getlastName()
-{
+  static getlastName() {
     return faker.person.lastName();
-    
-}
 
+  }
 
-static getFullName()
-{
+  static getFullName() {
     return faker.person.fullName();
-    
-}
 
-static getEmail()
-{
+  }
+
+  static getEmail() {
     return faker.internet.email();
 
-}
+  }
 
-static getPhoneNumber()
-{
+  static getPhoneNumber() {
     return faker.phone.number();
 
-}
+  }
 
- static getUsername(): string {
+  static getUsername(): string {
     return faker.internet.username();
 
   }
 
   static getPassword(): string {
     return faker.internet.password();
- }
+  }
 
- 
+
   static getRandomCountry(): string {
     return faker.location.country();
   }
 
-  
-    static getRandomState(): string {
+
+  static getRandomState(): string {
     return faker.location.state();
   }
 
@@ -56,15 +50,15 @@ static getPhoneNumber()
     return faker.location.city();
   }
 
- static getRandomPin(): string {
+  static getRandomPin(): string {
     return faker.location.zipCode();
   }
 
-    
- static getRandomAddress(): string {
+
+  static getRandomAddress(): string {
     return faker.location.streetAddress();
   }
-  
+
   static getRandomPassword(length: number = 10): string {
     return faker.internet.password({ length });
   }
